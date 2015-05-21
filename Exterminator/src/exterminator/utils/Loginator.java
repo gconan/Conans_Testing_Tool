d2a21d13f89cbb5d1f6ea83e2e47f08fa9546e1b
gconan <gconan@utexas.edu>
2015-05-21 16:33:48 -0500

    first test for loginator written

---
+++ Exterminator/src/exterminator/utils/Loginator.java
@@ -0,0 +1,23 @@
+package exterminator.utils;
+
+/**
+ * Named Loginator after the Terminator.
+ * @author conangammel
+ *
+ */
+public class Loginator {
+	
+	
+	public Loginator(){
+		
+	}
+	
+	public void log(String msg){
+		
+	}
+	
+	public void setFileName(String name){
+		
+	}
+
+}
---
+++ Exterminator/src/exterminator/utils/test/Loginator_Tester.java
@@ -0,0 +1,74 @@
+package exterminator.utils.test;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import exterminator.utils.Loginator;
+
+public class Loginator_Tester {
+
+	public File file;
+	public String fileName = "LoginatorTesterFile";
+	public Loginator logger;
+	
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		logger = new Loginator();
+		logger.setFileName(fileName);
+		file  = new File(fileName);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSimpleLog() {
+		String s = "this is working!";
+		logger.log(s);
+		assertTrue(fileContains(s));
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	public boolean fileContains(String s){
+		Scanner reader = null;
+		try {
+			reader = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			return false;
+		}
+		
+		boolean exists = false;
+		
+		while(reader.hasNextLine()){
+			exists |= reader.nextLine().equals(s);
+			reader.close();
+		}
+		return exists;
+	}
+
+}
